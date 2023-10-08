63d5fe22cc0d5334808d22074fe15c4ea4b6a0e7
liam-sh <liamsh@gmail.com>
2023-10-07 18:01:32 -0700

    profile route api call

--- src/routes/profile/index.jsx
+++ src/routes/profile/index.jsx
@@ -1,13 +1,32 @@
 import { useParams } from "react-router";
+import { db } from "src/setup/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 function Profile() {
   const {userid} = useParams()
+  const [userObj, setUserObj] = useState(null)
+
+  useEffect(()=>{
+    const getUser = async () => {
+      const docRef = doc(db, "users", userid);
+      console.log(docRef)
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setUserObj(docSnap.data())
+      } else {
+        console.log(`userid:${userid} not found`)
+      }
+    }
+    getUser()
+  }, [userid])
   
 
   return (
     <div>
       <h1>Profile</h1>
-      <p>{userid}</p>
+      <p>{userObj ? userObj.name : ''}</p>
     </div>
   );
 }
